3d4c818448351f95fd0c83e879daa24dfd55c1bf
cristianKumul <cristian.kumul@gmail.com>
2017-04-06 00:57:07 -0500

    Templates y comunicación entre content script y popupp
    comunicación

--- popup/popup.js
+++ popup/popup.js
@@ -1,22 +1,57 @@
 (function () {
     'use strict';
+    var conf = {
+        notify:".notify",
+        nToDownload:"#toDownloadNumber",
+        nInvoiced:"#invoicedNumber",
+        downloadedContainer: "#invoiceDownloaded",
+        toDownloadContainer:"#toDownload"
+    }
+    var findXmlAction=function(response){
+        Zepto(conf.notify).hide();
+        Zepto(conf.nInvoiced).html("");
+        Zepto(conf.nInvoiced).append(response.status);
+        Zepto(conf.downloadedContainer).show();
+    }
+    var checkXmlAction=function(response){
+        Zepto(conf.notify).hide();
+        if(response.status > 0){
+        Zepto(conf.nToDownload).html(response.status);
+            Zepto(conf.toDownloadContainer).show();
+        }else{
+             Zepto("#backbtn").show();
+        }
+        
+    }
 
-
-    var backgroundpage = chrome.extension.getBackgroundPage();
+    var callBackManager = function(response){
+        switch (response.message) {
+            case "findXML":
+                console.log(response.message + " " + JSON.stringify(response));
+                break;
+            case "checkXML":
+                console.log(response.message + " " + JSON.stringify(response));
+                checkXmlAction(response);
+            default:
+                break;
+        }
+    }
     var messenger = function(message){
-
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {action: message}, function(response) {
-            console.log(response.result);
+             console.log(response.result);
+             callBackManager(response.result);
             });
         });
 
     }
-    document.querySelector('button').addEventListener('click', function () {
-        console.log("click desde afyera");
+    document.querySelector('#button').addEventListener('click', function () {
        messenger("findXML");
     });
-    
+    document.querySelector('#other').addEventListener('click', function () {
+        messenger("checkXML");
+        
+    });   
   
     
 }());
--- program/action.js
+++ program/action.js
@@ -1,8 +1,29 @@
+
 (function () {
     'use strict';
+    var downloadFile = function(uri){
+        var link = document.createElement("a");
+        link.download = "name.xml";
+        link.href = uri;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        
+    }
     var parseUrl =  function(actionUrl){
         let link =  actionUrl.replace("return AccionCfdi('","").replace("','Recuperacion');","");
         return link;
+    }
+    var getXMLNumbers = function(){
+       var imgQuery = document.querySelector("#ctl00_MainContent_tblResult");
+       var numbers = 0;
+       if(imgQuery != undefined){
+            var downloadBtn = imgQuery.getElementsByClassName("BtnDescarga");
+            if(downloadBtn != undefined ){
+                numbers = downloadBtn.length;
+            }
+       }
+        return numbers;  
     }
     var findXML= function(){
         console.log("click to button");
@@ -14,49 +35,45 @@
             if(downloadBtn != undefined){
                 for(let i = 0 ; i < downloadBtn.length ; i++){
                     let link = baseUrl+parseUrl(downloadBtn[i].attributes[6].value) ;
-
+                    downloadFile(link);
                     listUrl.push(link);
                  }            
-                console.log(listUrl[0]);
-                chrome.downloads.download({
-                    url: listUrl[0],
-                    filename: "suggested/filename/with/relative.xml" // Optional
-                },function(){
-                    console.log("listo");
-                });
+                return listUrl.length;
             }
+            return 0;
             //sendResponse({result: "OK"});
         } catch (error) {
+            return -1;
             console.log("Error while try to handleButtonClick",error.message);
             //sendResponse({result: "Error"});
         }
     }
 
-    console.log("hola content");
+   
 
+    var managerActions = function(key){
+        switch (key) {
+            case "findXML":
+                return findXML();
+                break;
+            case "checkXML" :  
+                return getXMLNumbers();     
+            default:      
+                break;
+        }
+    }
 
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
-            console.log(sender.tab ?
-                    "from a content script:" + sender.tab.url :
-                    "from the extension");
-
-            if(request.action == "findXML"){
-                console.log(request.action);
-                findXML();
-            }
-            /*switch (request.action) {
-                case "findXML":
-                    findXML();
-
-                    break;
-            
-                default:
-                    console.log("default action");
-                    break;
-            }*/
-            
-             
+            console.log(sender.tab ?"from a content script:" + sender.tab.url :  "from the extension");
+            let response = managerActions(request.action);  
+            var results = {
+                    result:
+                        {  message: request.action,
+                           status: response}
+                    };
+    console.log(results);
+            sendResponse(results);
     });
    
 }());
